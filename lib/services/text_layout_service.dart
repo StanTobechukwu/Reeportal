974340a5faecbc6c5ac82e0101d2b4aa4a3d3e53
StanTobechukwu <nduagubatobechukwu@gmail.com>
2025-03-31 23:16:01 +0100

    Added text layout service

---
+++ lib/services/text_layout_service.dart
@@ -0,0 +1,8 @@
+class TextLayoutService {
+  List<TextSpan> applyTextWrapping({
+    required String text,
+    required List<DocumentElement> elements,
+  }) {
+    // Implementation for text wrapping logic
+  }
+}
